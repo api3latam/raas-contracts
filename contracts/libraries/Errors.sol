551482776833b777ca0598180fceb65f9f2ba284
gpablo3 <giancarlo@api3.org>
2022-12-03 17:05:30 -0600

    Add Errors library

---
+++ contracts/libraries/Errors.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+/**
+ * @title Errors
+ * @author API3 Latam
+ * 
+ * @notice A standard library of error types used across the API3 LATAM
+ * Raffle Platform.
+ */
+library Errors {
+
+    // Modules Airnodes
+    error RequestIdNotKnown();
+    error InvalidWinnerNumber(); // WinnerAirnode
+
+}
