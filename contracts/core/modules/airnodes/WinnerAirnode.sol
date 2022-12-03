643059d3311ab33d7eaae53a94570119c64f2a5a
gpablo3 <giancarlo@api3.org>
2022-12-03 17:02:31 -0600

    WIP add first callback for individual winner

--- contracts/core/modules/airnodes/WinnerAirnode.sol
+++ contracts/core/modules/airnodes/WinnerAirnode.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.15;
 
 import { DataTypes } from "../../../libraries/DataTypes.sol";
 import { Events } from "../../../libraries/Events.sol";
+import { Errors } from "../../../libraries/Errors.sol";
 
 import { AirnodeLogic } from "../../base/AirnodeLogic.sol";
 
@@ -16,6 +17,8 @@ import { AirnodeLogic } from "../../base/AirnodeLogic.sol";
  */
 contract WinnerAirnode is AirnodeLogic {
 
+    mapping(bytes32 => DataTypes.WinnerReponse) internal requestToRaffle; // Raffle airnode metadata for each request.
+
     constructor (
         address _airnodeRrpAddress
     ) AirnodeLogic(
@@ -23,26 +26,18 @@ contract WinnerAirnode is AirnodeLogic {
     ) {}
 
     /**
-     * @notice
+     * @notice Core logic for calling airnode rrp protocol.
+     *
+     * @param _endpointIdIndex - The target endpoint to use as callback.
+     * @param _parameters - The payload for the API from the airnode.
      */
     function callAirnode (
-        uint256 endpointIdIndex,
+        uint256 _endpointIdIndex,
         bytes calldata _parameters
-    ) external override returns (
+    ) internal override returns (
         bytes32
     ) {
         DataTypes.Endpoint memory currentEndpoint = endpointIds[endpointIdIndex];
-        bytes parameters;
-
-        require(
-            _parameters.length >= 0
-        );
-            
-        if (_parameters.length == 0) {
-            parameters = "";
-        } else if (_parameters.length > 0) {
-            parameters = _parameters;
-        }
 
         bytes32 _requestId = airnodeRrp.makeFullRequest(
             airnode,
@@ -51,26 +46,77 @@ contract WinnerAirnode is AirnodeLogic {
             sponsorWallet,
             address(this),
             currentEndpoint.functionSelector,
-            parameters
+            _parameters
         );
 
         incomingFulfillments[_requestId] = true;
-        requestToEndpoint[_requestId] = endpointIdIndex;
-        
-        emit Events.NewRequest (
-            _requestId,
-            currentEndpoint.endpointId,
-            airnode
-        );
     }
 
     /**
-     * @notice - 
+     * @notice - The interface to request this airnode implementation call.
+     *
+     * @param endpointIdIndex - The target endpoint to use as callback.
+     * @param winnerNumbers - The number of winners to return
+     * @param participantNumbers - The number of participants from the raffle.
      */
-    function pickOneWinner (
-        bytes32 requestId, 
+    function requestWinners (
+        uint256 endpointIdIndex,
+        uint256 winnerNumbers,
+        uint256 participantNumbers
+    ) external returns (
+        bytes32
+    ) {
+        bytes memory parameters;
+
+        if (winnerNumbers < 0) {
+            revert Errors.InvalidWinnerNumber();
+        } else if (winnerNumbers == 1) {
+            parameters = "";
+        } else {
+            parameters = abi.encode(bytes32("1u"), bytes32("size"), winnerNumbers);
+        }
+
+        bytes32 requestId = callAirnode (
+            endpointIdIndex,
+            parameters
+        );
+
+        DataTypes.WinnerReponse memory initResponse = DataTypes.WinnerReponse(
+            winnerNumbers,
+            participantNumbers,
+            uint256[winnerNumbers]
+        );
+        requestToRaffle[requestId] = initResponse;
+
+        emit Events.NewWinnerRequest(
+            requestId,
+            airnode
+        );
+
+        return requestId;
+    }
+
+    /**
+     * @notice - Callback function when requesting one winner only.
+     * @dev - We suggest to set this as endpointId index `1`.
+     *
+     * @param requestId - The id for this request.
+     * @param data - The response from the API send by the airnode.
+     */
+    function getIndividualWinner (
+        bytes32 requestId,
         bytes calldata data
     ) external override onlyAirnodeRrp {
-        uint256 
+        if (incomingFulfillments[requestId] != true) {
+            revert Errors.RequestIdNotKnown();
+        }
+
+        uint256 qrngUint256 = abi.decode(data, (uint256));
+
+        DataTypes.WinnerReponse memory raffleData = requestToRaffle[requestId];
+
+        uint256 winnerIndex = qrngUint256 % raffleData.totalEntries;
+
+        requestToRaffle[requestId].winnerIndexes.push(winnerIndex);
     }
 }
