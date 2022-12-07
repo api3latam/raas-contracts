5fa4c6be034aa77b899a5c02bda9245a3dd4cce1
gpablo3 <giancarlo@api3.org>
2022-12-06 21:21:38 -0600

    Add IRaffle interface

---
+++ contracts/interfaces/IRaffle.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.15;
+
+interface IRaffle {
+
+    /**
+     * @notice Enter the raffle.
+     * @param participantAddress The participant address.
+     */
+    function enter (
+        address participantAddress
+    ) external;
+
+    /**
+     * @notice Closes the ongoing raffle.
+     * @dev Called by the owner when the raffle is over.
+     * This function stops new entries from registering and will
+     * call the `WinnerAirnode`.
+     */
+    function close () external;
+
+    /**
+     * @notice Get the participants from the raffle.
+     */
+    function getEntries () 
+     external view returns (
+        address[] memory
+    );
+
+    /**
+     * @notice Get the winner addresses.
+     */
+    function getWinner ()
+     external view returns (
+        address[] memory
+     )
+
+}
