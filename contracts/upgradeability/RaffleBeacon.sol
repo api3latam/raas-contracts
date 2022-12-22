707dd0957fe3d42df692c98c52073e3d92208d80
gpablo3 <giancarlo@api3.org>
2022-12-22 16:00:15 -0600

    Update Raffle Beacon to inheritance instead of factoring

--- contracts/upgradeability/RaffleBeacon.sol
+++ contracts/upgradeability/RaffleBeacon.sol
@@ -9,32 +9,16 @@ import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
  * @title RaffleBeacon
  * @author API3 Latam
  *
- * @notice Beacon to be used for proxies when creating a raffle from hub.
- * It points to the latest raffle implementation.
+ * @notice Beacon to be used as reference for beacon proxies 
+ * when creating a raffle from hub.
+ * @dev It should point to the latest raffle implementation.
  */
-contract RaffleBeacon is Ownable {
-
-    UpgradeableBeacon immutable beacon;
-    address public currentImplementation;
+contract RaffleBeacon is UpgradeableBeacon {
 
     constructor (
         address _initialImplementation
-    ) {
-        beacon = new UpgradeableBeacon(raffleImplementation);
-        transferOwnership(tx.origin);
-        currentImplementation = raffleImplementation;
-    }
-
-    /**
-     * @notice Updates the raffle implementation at the beacon.
-     *
-     * @param _newImplementation The new contract address for the raffle logic.
-     */
-    function updateImplementation(
-        address _newImplementation
-    ) external onlyOnwer {
-        beacon.upgradeTo(_newImplementation);
-        currentImplementation = _newImplementation;
-    }
+    ) UpgradeableBeacon (
+        _initialImplementation
+    ) {}
 
 }
