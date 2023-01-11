715abef7f439b48aad38e06bbb7d328822b15d36
gpablo3 <giancarlo@api3.org>
2023-01-11 16:23:23 -0600

    Add interface for AssetVault

---
+++ contracts/interfaces/IAssetVault.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+/**
+ * @title IAssetVault
+ * @author API3 Latam
+ *
+ * @notice This is the interface for the AssetVault contract,
+ * which is utilized for safekeeping of assets during Hub
+ * user flows, like 'Raffles'.
+ *
+ * This contract is based from the arcadexyz repository `v2-contracts`.
+ * You can found it at this URL:
+ * https://github.com/arcadexyz/v2-contracts/blob/main/contracts/interfaces/IAssetVault.sol
+ */
+interface IAssetVault {
+
+    // ========== Core Functions ==========
+    /**
+     * @notice Enables withdrawals on the vault.
+     * @dev Any integration should be aware that a withdraw-enabled vault cannot
+     * be transferred (will revert).
+     */
+    function enableWithdraw () 
+     external;
+
+    /**
+     * @notice Withdraw entire balance of a given ERC721 token from the vault.
+     * The vault must be in a "withdrawEnabled" state (non-transferrable).
+     * The specified token must exist and be owned by this contract.
+     *
+     * @param token The token to withdraw.
+     * @param tokenId The ID of the NFT to withdraw.
+     * @param to The recipient of the withdrawn token.
+     */
+    function withdrawERC721 (
+        address token,
+        uint256 tokenId,
+        address to
+    ) external;
+
+    // ========== Get/Set Functions ==========
+    /**
+     * 
+     */
+    function withdrawEnabled ()
+     external view returns (
+        bool
+    );
+}
